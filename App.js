1ba8266b08775dfee3977fab8c8d4d5abad52558
Junior <junior.jr.jl@gmail.com>
2023-01-07 11:49:48 -0300

    página de regras

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Start from './src/pages/Start';
+import Rules from './src/pages/Start/Rules';
 
-const App = () => <Start />
+const App = () => 
+    <>
+        <StatusBar color="light"/>
+        <Rules />
+    </>
 
 export default App
