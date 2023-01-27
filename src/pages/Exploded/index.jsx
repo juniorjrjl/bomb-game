07182828dcf07892835cd08381bd69093de3457d
Junior <junior.jr.jl@gmail.com>
2023-01-26 23:13:55 -0300

    ajuste do nome do app e vibração na tela de explosão

--- src/pages/Exploded/index.jsx
+++ src/pages/Exploded/index.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Container, Logo, FailedImg, Title } from "./styled";
 
 import logoImg from '../../assets/logoLightRed.png';
 import failedImg from '../../assets/bomba_explodiu.png';
 import ButtonComponent from "../../components/Buttons";
+import { Vibration } from "react-native";
 
 const Explouded = () =>{
     const navigation = useNavigation()
     const handleNavToStart = () => navigation.navigate("Start")
+
+    useEffect(() =>{
+        Vibration.vibrate(5000)
+    }, [])
+
     return(
         <Container>
             <Logo source={logoImg} style={{ resizeMode: "contain" }} />
